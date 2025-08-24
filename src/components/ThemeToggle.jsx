d821d60bf1f62ad4460cd38347ee4dcbc0f6c49d
georgie5 <2021154499@ub.edu.bz>
2025-08-24 02:02:47 -0600

    improve ThemeToggle button

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -29,7 +29,14 @@ export const ThemeToggle = () => {
   };
 
   return (
-    <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300", "focus:outlin-hidden")}>
+    <button
+      onClick={toggleTheme}
+      className={cn(
+        "fixed z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outline-none",
+        "top-5 right-5", // Default position for larger screens
+        "max-sm:bottom-5 max-sm:right-5 max-sm:top-auto" // Adjust position for small screens
+      )}>
       {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" /> : <Moon className="h-6 w-6 text-blue-900" />}
     </button>
   );
